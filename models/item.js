0125cc10b68b680fb3c24f91c00abda9189d72e5
opashaj5 <opashaj5@yahoo.com>
2022-05-04 19:33:30 -0500

    first commmit

---
+++ models/item.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const itemSchema = require('./itemSchema')
+
+module.exports = mongoose.model('Item', itemSchema);
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import styles from './Logo.module.css';
+
+export default function Logo() {
+    return (
+        <div className={styles.Logo}>
+            <div>Peas in a Pod</div>
+        </div>
+    );
+}
